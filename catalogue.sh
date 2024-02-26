19eb624a6f45ac1e319353320373298fff6076db
Eliezer <eliezersov@gmail.com>
2024-02-26 17:05:55 +0530

    frontend.sh script

--- catalogue.sh
+++ catalogue.sh
@@ -20,4 +20,4 @@ systemctl start catalogue
 cp ${script_location}/files/mongodb.repo /etc/yum.repos.d/mongodb.repo
 dnf install mongodb-org-shell -y
 
-mongo --host localhost </app/schema/catalogue.js
+mongo --host mongodb-dev.perfectandupright.online </app/schema/catalogue.js
