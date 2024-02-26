155a21f94b2d4395528bcdaf62c7c63b7a9701d1
Eliezer <eliezersov@gmail.com>
2024-02-27 00:51:39 +0530

    frontend.sh script

--- frontend.sh
+++ frontend.sh
@@ -3,23 +3,31 @@ LOG=/tmp/roboshop.log
 
 echo -e "\e[35m Install Nginx\e[0m"
 dnf install nginx -y  &>>${LOG}
+echo $?
 
 echo -e "\e[35m Remove Old Nginx Content\e[0m"
 rm -rf /usr/share/nginx/html/*  &>>${LOG}
+echo $?
 
 echo -e "\e[35m Download Frontend Content\e[0m"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip  &>>${LOG}
+echo $?
 
 cd /usr/share/nginx/html  &>>${LOG}
+echo $?
 
 echo -e "\e[35m Extract Nginx Content\e[0m"
 unzip /tmp/frontend.zip  &>>${LOG}
+echo $?
 
 echo -e "\e[35m Copy Roboshop Nginx RoboShop Config File\e[0m"
 cp ${script_location}/files/nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf  &>>${LOG}
+echo $?
 
 echo -e "\e[35m enable Nginx\e[0m"
 systemctl enable nginx  &>>${LOG}
+echo $?
 
 echo -e "\e[35m Start Nginx\e[0m"
 systemctl restart nginx  &>>${LOG}
+echo $?
