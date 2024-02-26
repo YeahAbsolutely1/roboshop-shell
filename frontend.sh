85332815e2741f3e1d731a90b5c8e8abe677290d
Eliezer <eliezersov@gmail.com>
2024-02-27 01:36:45 +0530

    frontend.sh shell-script

--- frontend.sh
+++ frontend.sh
@@ -3,31 +3,59 @@ LOG=/tmp/roboshop.log
 
 echo -e "\e[35m Install Nginx\e[0m"
 dnf install nginx -y  &>>${LOG}
-echo $?
+if [ $? -eq 0]; then
+  echo SUCCESS
+else
+  echo FAILURE
+fi
 
 echo -e "\e[35m Remove Old Nginx Content\e[0m"
 rm -rf /usr/share/nginx/html/*  &>>${LOG}
-echo $?
+if [ $? -eq 0]; then
+  echo SUCCESS
+else
+  echo FAILURE
+fi
 
 echo -e "\e[35m Download Frontend Content\e[0m"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip  &>>${LOG}
-echo $?
+if [ $? -eq 0]; then
+  echo SUCCESS
+else
+  echo FAILURE
+fi
 
 cd /usr/share/nginx/html  &>>${LOG}
-echo $?
 
 echo -e "\e[35m Extract Nginx Content\e[0m"
 unzip /tmp/frontend.zip  &>>${LOG}
-echo $?
+if [ $? -eq 0]; then
+  echo SUCCESS
+else
+  echo FAILURE
+fi
+
 
 echo -e "\e[35m Copy Roboshop Nginx RoboShop Config File\e[0m"
 cp ${script_location}/files/nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf  &>>${LOG}
-echo $?
+if [ $? -eq 0]; then
+  echo SUCCESS
+else
+  echo FAILURE
+fi
 
 echo -e "\e[35m enable Nginx\e[0m"
 systemctl enable nginx  &>>${LOG}
-echo $?
+if [ $? -eq 0]; then
+  echo SUCCESS
+else
+  echo FAILURE
+fi
 
 echo -e "\e[35m Start Nginx\e[0m"
 systemctl restart nginx  &>>${LOG}
-echo $?
+if [ $? -eq 0]; then
+   echo SUCCESS
+ else
+   echo FAILURE
+ fi
