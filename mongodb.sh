3923cca19ae8e218c84a462a524d24c117ff7bb8
Eliezer <eliezersov@gmail.com>
2024-02-28 01:52:28 +0530

    mongodb.sh

--- mongodb.sh
+++ mongodb.sh
@@ -1,21 +1,21 @@
 source common.sh
 
 print_head "Copy MongoDB Repo"
-cp ${script_location}/files/mongodb.repo /etc/yum.repos.d/mongodb.repo $>>${LOG}
+cp ${script_location}/files/mongodb.repo /etc/yum.repos.d/mongodb.repo &>>${LOG}
 status_check
 
 print_head "Install MongoDB"
-dnf install mongodb-org -y $>>${LOG}
+dnf install mongodb-org -y &>>${LOG}
 status_check
 
 print_head "Update MongoDB Listen Address"
-sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf $>>${LOG}
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf &>>${LOG}
 status_check
 
 print_head "Enable MongoDB"
-systemctl enable mongod $>>${LOG}
+systemctl enable mongod &>>${LOG}
 status_check
 
 print_head "Start MongoDB"
-systemctl restart mongod $>>${LOG}
+systemctl restart mongod &>>${LOG}
 status_check
