7fe25bcfb80d856049b2cdd0308068602c0db50c
Eliezer <eliezersov@gmail.com>
2024-02-28 00:56:38 +0530

    catalogue.sh

--- common.sh
+++ common.sh
@@ -4,9 +4,9 @@ LOG=/tmp/roboshop.log
 status_check() {
   if [ $? -eq 0 ]
    then
-     echo -e "\e[32mSUCCESS\0m"
+     echo -e "\e[1;32mSUCCESS\0m"
    else
-     echo -e "\e[31mFAILURE\0m"
+     echo -e "\e[1;31mFAILURE\0m"
      echo 'Please refer to a long file for more information. LOG - ${LOG}'
   exit
   fi
